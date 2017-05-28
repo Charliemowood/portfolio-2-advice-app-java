707baa74c133a309aa589484e8155ad4f155eb6e
Charlie Wood <charliemowood@gmail.com>
2017-05-29 00:24:15 +0100

    Add dropdown submit and app logic to choose smart answer based on user response

--- app/src/main/java/com/codeclan/example/adviceapp/AdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/AdviceChoiceActivity.java
@@ -1,13 +1,48 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Objects;
 
 public class AdviceChoiceActivity extends AppCompatActivity {
+    private Button houseButton;
+    private HouseAnswerProvider answers;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advice_choice);
+        houseButton = (Button) findViewById(R.id.house_button);
+        spinner = (Spinner) findViewById(R.id.house_spinner);
+    }
+
+    public void onHouseButtonClicked(View button) {
+        Log.d(getClass().toString(), "Shake button was clicked");
+        HouseAnswerProvider houseAnswerProvider = new HouseAnswerProvider();
+        String text = spinner.getSelectedItem().toString();
+        if (Objects.equals(text, "I like to be at one with nature.")) {
+            String answer = houseAnswerProvider.getAnswerAtIndex(0);
+            getAnswerOpenIntent(answer);
+        } else if (Objects.equals(text, "I am not afraid of fires.")) {
+            String answer = houseAnswerProvider.getAnswerAtIndex(1);
+            getAnswerOpenIntent(answer);
+        } else {
+            String answer = houseAnswerProvider.getAnswerAtIndex(2);
+            getAnswerOpenIntent(answer);
+        }
+
+    }
+
+    private void getAnswerOpenIntent(String answer) {
+        Intent intent = new Intent(this, AnswerActivity.class);
+        intent.putExtra("answer", answer);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/codeclan/example/adviceapp/HouseAnswerProvider.java
+++ app/src/main/java/com/codeclan/example/adviceapp/HouseAnswerProvider.java
@@ -1,6 +1,7 @@
 package com.codeclan.example.adviceapp;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by home on 5/28/17.
@@ -9,11 +10,46 @@ import java.util.ArrayList;
 public class HouseAnswerProvider {
     private ArrayList<String> answers;
 
+    public HouseAnswerProvider(ArrayList<String> answers) {
+        this.answers = answers;
+    }
+
     public HouseAnswerProvider() {
         this.answers = new ArrayList<>();
+        setupDefaultAnswers();
+    }
+
+    private void setupDefaultAnswers() {
+        String[] defaultAnswers = {
+                "You should build a house with earth bags.",
+                "You should build a house made of wood.",
+                "You should build a house made of bricks."
+        };
+        for (String answer : defaultAnswers) {
+            this.answers.add(answer);
+        }
     }
 
     public ArrayList<String> getAnswers() {
         return new ArrayList<>(this.answers);
+
+    }
+
+    public int getNumberOfAnswers() {
+        return this.answers.size();
+    }
+
+    public void add(String answer) {
+        this.answers.add(answer);
+    }
+
+    public String getAnswerAtIndex(int i) {
+        return this.answers.get(i);
+    }
+
+    public String getAnswer() {
+        Random rand = new Random();
+        int index = rand.nextInt(getNumberOfAnswers());
+        return getAnswerAtIndex(index);
     }
 }
