2903a1b393e621f09c1ce1a0b1a112672a59eee7
Charlie Wood <charliemowood@gmail.com>
2017-05-29 21:43:46 +0100

    Add tests and build MarriageAnswerProviderClass

--- app/src/main/java/com/codeclan/example/adviceapp/MarriageAnswerProvider.java
+++ app/src/main/java/com/codeclan/example/adviceapp/MarriageAnswerProvider.java
@@ -1,8 +1,54 @@
 package com.codeclan.example.adviceapp;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * Created by home on 5/29/17.
  */
 
 public class MarriageAnswerProvider {
+
+    private ArrayList<String> answers;
+
+    public MarriageAnswerProvider() {
+        this.answers = new ArrayList<>();
+        setupDefaultAnswers();
+    }
+
+    private void setupDefaultAnswers() {
+        String[] defaultAnswers = {
+                "Yep, definitely get married!",
+                "You need time time to make sure you are sure.",
+                "Sorry, this is not the right time to get married."
+        };
+        for (String answer : defaultAnswers) {
+            this.answers.add(answer);
+        }
+    }
+
+    public ArrayList<String> getAnswers() {
+        return new ArrayList<>(this.answers);
+
+    }
+
+    public int getNumberOfAnswers() {
+        return this.answers.size();
+    }
+
+    public void add(String answer) {
+        this.answers.add(answer);
+    }
+
+    public String getAnswerAtIndex(int i) {
+        return this.answers.get(i);
+    }
+
+    public String getAnswer() {
+        Random rand = new Random();
+        int index = rand.nextInt(getNumberOfAnswers());
+        return getAnswerAtIndex(index);
+    }
+
+
 }
--- app/src/test/java/com/codeclan/example/adviceapp/MarriageAnswerProviderTest.java
+++ app/src/test/java/com/codeclan/example/adviceapp/MarriageAnswerProviderTest.java
@@ -1,10 +1,40 @@
 package com.codeclan.example.adviceapp;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by home on 5/29/17.
  */
 public class MarriageAnswerProviderTest {
+
+    @Test
+    public void answerAreNotNull() {
+        MarriageAnswerProvider provider = new MarriageAnswerProvider();
+        assertNotNull(provider.getAnswers());
+    }
+
+    @Test
+    public void defaultAnswer1() {
+        MarriageAnswerProvider provider = new MarriageAnswerProvider();
+        assertEquals("Yep, definitely get married!", provider.getAnswerAtIndex(0));
+    }
+
+    @Test
+    public void defaultAnswer2() {
+        MarriageAnswerProvider provider = new MarriageAnswerProvider();
+        assertEquals("You need time time to make sure you are sure.", provider.getAnswerAtIndex(1));
+    }
+
+    @Test
+    public void defaultAnswer3() {
+        MarriageAnswerProvider provider = new MarriageAnswerProvider();
+        assertEquals("Sorry, this is not the right time to get married.", provider.getAnswerAtIndex(2));
+    }
+
+
+
+
 
 }
