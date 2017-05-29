c13a5f8881a35b6e51951b075c246d44d0913878
Charlie Wood <charliemowood@gmail.com>
2017-05-29 22:29:44 +0100

    Add CareerAnswerProvider class and respective test

---
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerProvider.java
@@ -0,0 +1,8 @@
+package com.codeclan.example.adviceapp;
+
+/**
+ * Created by home on 5/29/17.
+ */
+
+public class CareerAnswerProvider {
+}
---
+++ app/src/test/java/com/codeclan/example/adviceapp/CareerAnswerProviderTest.java
@@ -0,0 +1,10 @@
+package com.codeclan.example.adviceapp;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by home on 5/29/17.
+ */
+public class CareerAnswerProviderTest {
+
+}
