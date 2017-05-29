0431cb3418b2f05325d2b19f33d72a1b15fc5f7b
Charlie Wood <charliemowood@gmail.com>
2017-05-29 23:05:23 +0100

    Add marriageButton functionality

--- app/src/main/java/com/codeclan/example/adviceapp/LauncherActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/LauncherActivity.java
@@ -8,16 +8,24 @@ import android.widget.Button;
 
 public class LauncherActivity extends AppCompatActivity {
     private Button buildButton;
+    private Button marriageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launcher);
         buildButton = (Button) findViewById(R.id.build_button);
+        marriageButton = (Button) findViewById(R.id.marriage_button);
     }
 
     public void onBuildButtonClick(View button) {
         Intent intent = new Intent(this, AdviceChoiceActivity.class);
         startActivity(intent);
+    }
+
+    public void onMarriageButtonClick(View button) {
+        Intent intent = new Intent(this, MarriageAdviceChoiceActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/codeclan/example/adviceapp/MarriageAdviceChoiceActivity.java
@@ -0,0 +1,16 @@
+package com.codeclan.example.adviceapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MarriageAdviceChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_marriage_advice_choice);
+    }
+
+}
