9c7448fb4781d95383a21c22b0aa4554dce43a69
Charlie Wood <charliemowood@gmail.com>
2017-05-29 22:37:53 +0100

    Add tests and functions for CareerAnswerProvider class

--- app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerProvider.java
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerProvider.java
@@ -1,8 +1,46 @@
 package com.codeclan.example.adviceapp;
 
+import java.util.ArrayList;
+
 /**
  * Created by home on 5/29/17.
  */
 
 public class CareerAnswerProvider {
+
+    private ArrayList<String> answers;
+
+    public CareerAnswerProvider() {
+        this.answers = new ArrayList<>();
+        setupDefaultAnswers();
+    }
+
+    private void setupDefaultAnswers() {
+        String[] defaultAnswers = {
+                "You should pursue a career using computers.",
+                "You should consider working as a gardener.",
+                "You might like to work as a professional gambler."
+        };
+        for (String answer : defaultAnswers) {
+            this.answers.add(answer);
+        }
+    }
+
+    public ArrayList<String> getAnswers() {
+        return new ArrayList<>(this.answers);
+
+    }
+
+    public int getNumberOfAnswers() {
+        return this.answers.size();
+    }
+
+    public void add(String answer) {
+        this.answers.add(answer);
+    }
+
+    public String getAnswerAtIndex(int i) {
+        return this.answers.get(i);
+    }
+
 }
--- app/src/test/java/com/codeclan/example/adviceapp/CareerAnswerProviderTest.java
+++ app/src/test/java/com/codeclan/example/adviceapp/CareerAnswerProviderTest.java
@@ -1,10 +1,36 @@
 package com.codeclan.example.adviceapp;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by home on 5/29/17.
  */
 public class CareerAnswerProviderTest {
+    @Test
+    public void answerAreNotNull() {
+        CareerAnswerProvider provider = new CareerAnswerProvider();
+        assertNotNull(provider.getAnswers());
+    }
+
+    @Test
+    public void defaultAnswer1() {
+        CareerAnswerProvider provider = new CareerAnswerProvider();
+        assertEquals("You should pursue a career using computers.", provider.getAnswerAtIndex(0));
+    }
+
+    @Test
+    public void defaultAnswer2() {
+        CareerAnswerProvider provider = new CareerAnswerProvider();
+        assertEquals("You should consider working as a gardener.", provider.getAnswerAtIndex(1));
+    }
+
+    @Test
+    public void defaultAnswer3() {
+        CareerAnswerProvider provider = new CareerAnswerProvider();
+        assertEquals("You might like to work as a professional gambler.", provider.getAnswerAtIndex(2));
+    }
+
 
 }
