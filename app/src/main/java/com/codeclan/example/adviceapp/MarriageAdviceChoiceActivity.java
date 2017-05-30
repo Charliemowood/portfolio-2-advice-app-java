00777d7988189a2ba417abbd20bd3ee67c7da3c1
Charlie Wood <charliemowood@gmail.com>
2017-05-30 14:15:39 +0100

    Add functionality of getting answer back from MarriageAnswerActivity

--- app/src/main/java/com/codeclan/example/adviceapp/MarriageAdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/MarriageAdviceChoiceActivity.java
@@ -3,14 +3,28 @@ package com.codeclan.example.adviceapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MarriageAdviceChoiceActivity extends AppCompatActivity {
+    Button marriageButtonSubmit;
+    Spinner spinnerMarriage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marriage_advice_choice);
+        marriageButtonSubmit = (Button) findViewById(R.id.marriage_submit_button);
+        spinnerMarriage = (Spinner) findViewById(R.id.marriage_spinner);
+    }
+
+    public void onMarriageButtonSubmitClick(View button) {
+        MarriageAnswerProvider provider = new MarriageAnswerProvider();
+        String answer = provider.getAnswerAtIndex(0);
+        Intent intent = new Intent(this, MarriageAnswerActivity.class);
+        intent.putExtra("answer", answer);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/codeclan/example/adviceapp/MarriageAnswerActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/MarriageAnswerActivity.java
@@ -1,13 +1,25 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MarriageAnswerActivity extends AppCompatActivity {
+
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marriage_answer);
+        answerText = (TextView)findViewById(R.id.marriage_answer_view);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String answer = extras.getString("answer");
+
+        answerText.setText(answer);
     }
 }
