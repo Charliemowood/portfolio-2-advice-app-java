74733449666a88383d5266016c574765898a3b4f
Charlie Wood <charliemowood@gmail.com>
2017-05-30 23:20:05 +0100

    Add career smart logic

--- app/src/main/java/com/codeclan/example/adviceapp/CareerAdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAdviceChoiceActivity.java
@@ -5,19 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Objects;
 
 public class CareerAdviceChoiceActivity extends AppCompatActivity {
     Button careerButton;
+    Spinner careerSpinner;
+    CareerAnswerProvider provider = new CareerAnswerProvider();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_advice_choice);
         careerButton = (Button)findViewById(R.id.career_answer_button);
+        careerSpinner = (Spinner)findViewById(R.id.spinner_buy);
     }
 
     public void onCareerButtonClick(View button) {
-        Intent intent = new Intent(this, CareerAnswerActivity.class);
+        String text = careerSpinner.getSelectedItem().toString();
+        if (Objects.equals(text, "I like to be at one with nature.")) {
+            String answer = provider.getAnswerAtIndex(0);
+            getAnswerOpenIntent(answer);
+        } else if (Objects.equals(text, "I am not afraid of fires.")) {
+            String answer = provider.getAnswerAtIndex(1);
+            getAnswerOpenIntent(answer);
+        } else {
+            String answer = provider.getAnswerAtIndex(2);
+            getAnswerOpenIntent(answer);
+        }
+    }
+
+    private void getAnswerOpenIntent(String answer) {
+        Intent intent = new Intent(this, AnswerActivity.class);
+        intent.putExtra("answer", answer);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerActivity.java
@@ -1,15 +1,24 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CareerAnswerActivity extends AppCompatActivity {
+    private TextView answerText;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_answer);
+        answerText = (TextView)findViewById(R.id.career_answer_textview);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String answer = extras.getString("answer");
+
+        answerText.setText(answer);
     }
 }
