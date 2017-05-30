e13b868b3537e1fe291f83dda33d469be44e8a34
Charlie Wood <charliemowood@gmail.com>
2017-05-30 22:50:51 +0100

    Add smart logic to buy something functionality

--- app/src/main/java/com/codeclan/example/adviceapp/BuyAdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/BuyAdviceChoiceActivity.java
@@ -5,18 +5,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+
+import java.util.Objects;
 
 public class BuyAdviceChoiceActivity extends AppCompatActivity {
-    Button buyButton;
+    private Button buyButton;
+    private Spinner buySpinner;
+    private BuyAnswerProvider provider = new BuyAnswerProvider();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_choice);
         buyButton = (Button)findViewById(R.id.buy_button);
+        buySpinner =(Spinner)findViewById(R.id.buy_spinner_input);
     }
     public void onBuyButtonClick(View button) {
+        String number = buySpinner.getSelectedItem().toString();
+        if (Objects.equals(number, "10")) {
+            String answer = provider.getAnswerAtIndex(0);
+            intentMethod(answer);
+        } else if (Objects.equals(number, "9")){
+            String answer = provider.getAnswerAtIndex(1);
+            intentMethod(answer);
+        } else {
+            String answer = provider.getAnswerAtIndex(2);
+            intentMethod(answer);
+        }
+
+    }
+
+    private void intentMethod(String answer) {
         Intent intent = new Intent(this, BuyAnswerActivity.class);
+        intent.putExtra("answer", answer);
         startActivity(intent);
     }
 
--- app/src/main/java/com/codeclan/example/adviceapp/BuyAnswerActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/BuyAnswerActivity.java
@@ -1,13 +1,22 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BuyAnswerActivity extends AppCompatActivity {
+    private TextView answerText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_answer);
+        answerText = (TextView)findViewById(R.id.buy_answer_result);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String answer = extras.getString("answer");
+
+        answerText.setText(answer);
     }
 }
