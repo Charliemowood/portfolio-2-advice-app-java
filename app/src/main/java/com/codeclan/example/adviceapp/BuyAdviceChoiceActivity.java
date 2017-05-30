b73eefca6919b7410c91914409de60b393bbd3d3
Charlie Wood <charliemowood@gmail.com>
2017-05-30 17:57:55 +0100

    Add button

--- app/src/main/java/com/codeclan/example/adviceapp/BuyAdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/BuyAdviceChoiceActivity.java
@@ -1,13 +1,23 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BuyAdviceChoiceActivity extends AppCompatActivity {
+    Button buyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_buy_choice);
+        buyButton = (Button)findViewById(R.id.buy_button);
     }
+    public void onBuyButtonClick(View button) {
+        Intent intent = new Intent(this, BuyAnswerActivity.class);
+        startActivity(intent);
+    }
+
 }
