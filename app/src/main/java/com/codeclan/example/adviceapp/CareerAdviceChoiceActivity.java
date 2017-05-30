b2239ec6f43e999b53e7d9c2fc1c7c992bfd4b2f
Charlie Wood <charliemowood@gmail.com>
2017-05-30 18:09:49 +0100

    Add appropriate navigation to buy and career activities

--- app/src/main/java/com/codeclan/example/adviceapp/CareerAdviceChoiceActivity.java
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAdviceChoiceActivity.java
@@ -1,13 +1,23 @@
 package com.codeclan.example.adviceapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CareerAdviceChoiceActivity extends AppCompatActivity {
+    Button careerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_career_advice_choice);
+        careerButton = (Button)findViewById(R.id.career_answer_button);
+    }
+
+    public void onCareerButtonClick(View button) {
+        Intent intent = new Intent(this, CareerAnswerActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/codeclan/example/adviceapp/CareerAnswerActivity.java
@@ -0,0 +1,15 @@
+package com.codeclan.example.adviceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class CareerAnswerActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_career_answer);
+    }
+}
